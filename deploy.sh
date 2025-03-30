1234e7596e924f0ae299074e686e2b0e008c01b1
Abhinav Srivastava <abhinav.022000@gmail.com>
2025-03-30 12:08:18 +0530

    Create deploy.sh
    Add deployment file.

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:abhinavsri360/calci.git master:gh-pages
+
+cd -
